66d28d498a6fa4beed4851ab549e0e6dada0376d
javicond3 <4b3javi@gmail.com>
2020-02-27 12:16:57 +0100

    add PAS people

--- back/code/controllers/gestion-titulos/permiso_controller.js
+++ back/code/controllers/gestion-titulos/permiso_controller.js
@@ -1,7 +1,10 @@
 //permisos específicos de la app
 exports.comprobarRolYPas = function (req, res, next) {
     if ((process.env.PRUEBAS == 'true' || process.env.DEV == 'true') ||
-        req.session.user.mail === "elena.garcia.leal@upm.es" || req.session.user.mail === "andres.cosano@upm.es") {
+        req.session.user.mail === "elena.garcia.leal@upm.es" || req.session.user.mail === "andres.cosano@upm.es" ||
+        req.session.user.mail === "mariaantonia.barquero@upm.es" || req.session.user.mail === "cristina.perezdeazpillaga@upm.es" ||
+        req.session.user.mail === "pilar.horcajada@upm.es" || req.session.user.mail === "esther.fuentes@upm.es" ||
+        req.session.user.mail === "mercedes.obispo@upm.es" || req.session.user.mail === "isabel.poza@upm.es") {
         next();
     } else {
         res.render('noPermitido');
