c75cb628149d8da3c0b821957def0e13005962ec
javicond3 <4b3javi@gmail.com>
2020-05-07 11:54:15 +0200

    add cors package

--- back/code/app.js
+++ back/code/app.js
@@ -4,6 +4,7 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let morgan = require('morgan');
 let session = require('express-session');
+const cors = require('cors');
 const MemoryStore = require('memorystore')(session);
 
 //dos contextos uno para pas y otro para alumnos
@@ -48,6 +49,12 @@ app.set('view engine', 'ejs');
 app.use(morgan('combined', {
   skip: function (req, res) { return res.statusCode < 400 }
 }))
+
+//only enable CORS in dev
+if (process.env.DEV == 'true') {
+//Enable CORS when developing react front
+app.use(cors());
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
