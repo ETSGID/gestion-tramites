bca60288f95dbf1abcf5f64348f0cb4352b50e54
IRENE <irene.garcialop@alumnos.upm.es>
2020-07-06 12:55:19 +0200

    nueva tabla peticiones evluacion curricular

---
+++ back/migrations/20200706093953-CreatePeticionEvaluacionCurricularsTable.js
@@ -0,0 +1,63 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Peticion_evaluacion_curriculars', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      edupersonuniqueid: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      planCodigo: {
+        type: Sequelize.STRING,
+      },
+      planNombre: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      nombre: {
+        type: Sequelize.STRING
+      },
+      apellido: {
+        type: Sequelize.STRING
+      },
+      estadoPeticion: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      fecha: {
+        type: Sequelize.DATEONLY
+      },
+      textCancel: {
+        type: Sequelize.TEXT
+      },
+      asignaturaNombre: {
+        type: Sequelize.STRING
+      },
+      asignaturaCodigo: {
+        type: Sequelize.STRING,
+      },
+      tipo: {
+        type: Sequelize.STRING
+      },
+      justificacion: {
+        type: Sequelize.STRING
+      }
+
+    },
+      {
+        timestamps: false
+      });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Peticion_evaluacion_curriculars')
+  }
+};
