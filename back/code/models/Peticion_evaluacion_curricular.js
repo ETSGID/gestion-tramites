b2600afe855e6f315b9d8e9a80e941eec5056d0b
IRENE <irene.garcialop@alumnos.upm.es>
2020-07-06 12:56:36 +0200

    nueva tabla peticion evaluacion curricular

---
+++ back/code/models/Peticion_evaluacion_curricular.js
@@ -0,0 +1,59 @@
+// Definicion del modelo Peticion:
+
+module.exports = function (sequelize, DataTypes) {
+    let Peticion = sequelize.define('Peticion_evaluacion_curricular',
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true
+            },
+            edupersonuniqueid: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            planCodigo: {
+                type: DataTypes.STRING,
+            },
+            planNombre: {
+                type: DataTypes.STRING
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            nombre: {
+                type: DataTypes.STRING
+            },
+            apellido: {
+                type: DataTypes.STRING
+            },
+            estadoPeticion: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            fecha: {
+                type: DataTypes.DATEONLY
+            },
+            textCancel: {
+                type: DataTypes.TEXT
+            },
+            asignaturaNombre: {
+                type: DataTypes.STRING
+            },
+            asignaturaCodigo: {
+                type: DataTypes.STRING,
+            },
+            tipo: {
+                type: DataTypes.STRING
+            },
+            justificacion: {
+                type: DataTypes.STRING
+            }
+
+        },
+        {
+            timestamps: false
+        });
+    return Peticion;
+};
--- back/code/models/index.js
+++ back/code/models/index.js
@@ -1,7 +1,5 @@
 let path = require('path');
 
-
-
 // Cargar ORM
 let Sequelize = require('sequelize');
 
@@ -12,31 +10,30 @@ let sequelize;
 sequelize = new Sequelize('postgres://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@' + process.env.DB_HOST + ':5432/' + process.env.POSTGRES_DB, { logging: logs });
 
 // Importar la definicion de las tablas 
-
 let Peticion = sequelize.import(path.join(__dirname, 'Peticion'));
+let PeticionEvaluacionCurricular = sequelize.import(path.join(__dirname, 'Peticion_evaluacion_curricular'));
 let Permiso = sequelize.import(path.join(__dirname, 'Permiso'));
-let Plan = sequelize.import(path.join(__dirname, 'Plan'))
+let Plan = sequelize.import(path.join(__dirname, 'Plan'));
 
+    // En producción ya no sincronizar, hacer mejor migraciones
+    // sequelize.sync();
 
-sequelize.sync();
+    // habilitar extension unaccent
+    // extension especifica de sequelize
 
-// habilitar extension unaccent
-// extension especifica de sequelize
-
-(async () => {
-    try {
-        await sequelize.query('CREATE EXTENSION unaccent;')
-    } catch (error) {
-        console.log(error.message);
-    }
-})();
-
-
+    (async () => {
+        try {
+            await sequelize.query('CREATE EXTENSION unaccent;')
+        } catch (error) {
+            console.log(error.message);
+        }
+    })();
 
 //Exportamos modelos
 
 exports.Peticion = Peticion;
 exports.Permiso = Permiso;
 exports.Plan = Plan;
+exports.PeticionEvaluacionCurricular = PeticionEvaluacionCurricular;
 
 exports.sequelize = sequelize;
