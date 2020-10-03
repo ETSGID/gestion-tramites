8244f17aab600adbc5f34db8dcf7f48cd260e5ff
IRENE <irene.garcialop@alumnos.upm.es>
2020-10-03 12:38:32 +0200

    añadidas ejecucion migraciones y seeders

--- script.sh
+++ script.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 echo "Hi, I'm sleeping until the database is up..."
 sleep 12
-npm start 
+echo "Executing migrations..."
+npm run migrations
+echo "Executing seeders..."
+npm run seeders
+npm start
 echo "all Done."
 exit 0
 
