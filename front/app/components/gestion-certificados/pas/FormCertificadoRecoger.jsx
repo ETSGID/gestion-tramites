556fbe31636b71e97ffe98c5a400a92368c75dcc
IRENE <irene.garcialop@alumnos.upm.es>
2020-10-03 11:19:12 +0200

    cambio a newCAS

--- front/app/components/gestion-certificados/pas/FormCertificadoRecoger.jsx
+++ front/app/components/gestion-certificados/pas/FormCertificadoRecoger.jsx
@@ -36,7 +36,7 @@ export default class FormTituloRecoger extends React.Component {
     return (
       <div>
         <Modal.Body>
-          Confirmar la recogida del certificado académico por parte de {this.props.peticion.nombre} {this.props.peticion.apellido} ({this.props.peticion.irispersonaluniqueid}).
+          Confirmar la recogida del certificado académico por parte de {this.props.peticion.nombre} {this.props.peticion.apellido} ({this.props.peticion.edupersonuniqueid}).
           <Form onSubmit={this.handleSubmit}>
             <Form.Group>
               <Form.Label as="legend">
