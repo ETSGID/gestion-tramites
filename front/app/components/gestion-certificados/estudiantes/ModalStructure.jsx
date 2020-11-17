e6b8e492c27d16ce0a69453643275837f90ac7c0
IRENE <irene.garcialop@alumnos.upm.es>
2020-11-17 12:55:17 +0100

    reiniciar solicitud desde cancelacion

--- front/app/components/gestion-certificados/estudiantes/ModalStructure.jsx
+++ front/app/components/gestion-certificados/estudiantes/ModalStructure.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Modal, Button, Form, Row, Col } from 'react-bootstrap';
-import FormPeticion from './FormPeticion';
+import { Modal } from 'react-bootstrap';
+import FormRestart from './FormRestart';
 import FormPago from './FormPago';
 const estadosCertificado = require('../../../../../back/enums').estadosCertificado;
 import InfoPeticion from './InfoPeticion';
@@ -38,6 +38,13 @@ export default class ModalStructure extends React.Component {
             cambioEstadoClick={this.cambioEstadoClick}
           >
           </FormPago>
+    } else if(estadosCertificado[this.props.peticion.estadoPeticionTexto] == estadosCertificado.PETICION_CANCELADA){
+      form = <FormRestart
+        peticion={this.props.peticion}
+        handleClose={this.handleClose}
+        cambioEstadoClick={this.cambioEstadoClick}
+      >
+      </FormRestart>
     }
     return (
       <Modal show={true} onHide={this.handleClose}>
