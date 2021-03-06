26180f78436ab06ad76c3f40b076043e78678ba8
javicond3 <4b3javi@gmail.com>
2021-03-06 18:15:35 +0100

    add matriculadoMaster, numVecesSuspensoSobre4 y log usuario

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## v2.0.5 - 2021-03-06
+
+### Added
+- Información adicional api evaluación curricular: matriculadoMaster, numVecesSuspensoSobre4
+- Log de la primera vez que el usario accede en la sesión
+
 ## v2.0.4 - 2021-02-21
 
 ### Added
--- front/app/components/evaluacion-curricular/pas/FormInfoRequisitos.jsx
+++ front/app/components/evaluacion-curricular/pas/FormInfoRequisitos.jsx
@@ -50,19 +50,23 @@ export default class FormInfoRequisitos extends React.Component {
     let textRequisitos = "Cargando..."
     if (this.state.datosCargados) {
       let estadoTFT = "no matriculado";
+      let matriculadoMaster = "no"
       if (this.state.data.aprobadoTFT) {
         estadoTFT = "aprobado"
       } else if (this.state.data.matriculadoTFT) {
         estadoTFT = "matriculado"
       }
+      if (this.state.data.matriculadoMaster) {
+        matriculadoMaster = "sí"
+      }
       let notasAnteriores = 'aún no hay notas anteriores registradas';
       if (this.state.data.notasAnteriores.length >= 2) {
         let length = this.state.data.notasAnteriores.length;
-        notasAnteriores = (<ul><li>{this.state.data.notasAnteriores[length-2].convocatoria} de {this.state.data.notasAnteriores[length-2].cursoAcademico}: {this.state.data.notasAnteriores[length-2].calificacionAlfa}-{this.state.data.notasAnteriores[length-2].calificacion}</li>
-          <li>{this.state.data.notasAnteriores[length-1].convocatoria} de {this.state.data.notasAnteriores[length-1].cursoAcademico}: {this.state.data.notasAnteriores[length-1].calificacionAlfa}-{this.state.data.notasAnteriores[length-1].calificacion}</li></ul>)
+        notasAnteriores = (<ul><li>{this.state.data.notasAnteriores[length - 2].convocatoria} de {this.state.data.notasAnteriores[length - 2].cursoAcademico}: {this.state.data.notasAnteriores[length - 2].calificacionAlfa}-{this.state.data.notasAnteriores[length - 2].calificacion}</li>
+          <li>{this.state.data.notasAnteriores[length - 1].convocatoria} de {this.state.data.notasAnteriores[length - 1].cursoAcademico}: {this.state.data.notasAnteriores[length - 1].calificacionAlfa}-{this.state.data.notasAnteriores[length - 1].calificacion}</li></ul>)
       } else if (this.state.data.notasAnteriores.length == 1) {
         notasAnteriores = (<ul><li>{this.state.data.notasAnteriores[0].convocatoria} de {this.state.data.notasAnteriores[0].cursoAcademico}: {this.state.data.notasAnteriores[0].calificacionAlfa}-{this.state.data.notasAnteriores[0].calificacion}</li>
-         </ul>)
+        </ul>)
       }
 
       let ultimaConvocatoria = 'aún no hay última convocatoria registrada';
@@ -86,12 +90,14 @@ export default class FormInfoRequisitos extends React.Component {
             <li>Nota media curso: {this.state.data.notaMediaCurso}</li>
             <li>Estado del TFT: {estadoTFT}</li>
             <li>ECTS pendientes: {this.state.data.creditosPendientes}</li>
+            <li>Matriculado Máster: {matriculadoMaster}</li>
           </ul>
           <b>Datos de la asignatura {this.props.peticion.asignaturaNombre} ({this.props.peticion.asignaturaCodigo})</b>
           <ul>
             <li>Número de veces suspensa en el curso anterior: {this.state.data.numVecesSuspensoCursoAnterior}</li>
             <li>Número de veces suspensa en el curso actual: {this.state.data.numVecesSuspensoCursoActual}</li>
             <li>Número de veces suspensa en total: {this.state.data.numVecesSuspenso}</li>
+            <li>Número de veces con nota superior a 4: {this.state.data.numVecesSuspensoSobre4}</li>
             <li>Fecha de última convocatoria: {ultimaConvocatoria}</li>
             <li>Dos últimas calificaciones: {notasAnteriores}</li>
           </ul>
